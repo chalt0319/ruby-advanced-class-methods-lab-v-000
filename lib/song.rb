e3583e4d636c7e8094d67929e31d9dfe4414eaae
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-25 16:46:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +10,41 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    person = self.new #Song.new
+    person.save 
+    person 
+  end
+  
+  def self.new_by_name(name)
+    person = self.new 
+    person.name = name 
+    person
+  end
+  
+  def self.create_by_name(name)
+    person = self.new 
+    person.name = name 
+    person.save 
+    person 
+  end 
+    
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    # binding.pry 
+    # self.create_by_name(name) unless self.find_by_name(name) != nil 
+    return_value = self.find_by_name(name) 
+    return_value ? return_value : self.create_by_name(name)
+    
+    # if self.find_by_name(name) 
+    #   self.find_by_name(name)  
+    # else 
+    #   self.create_by_name(name) 
+    # end 
+  end 
+  
 end
